74e1e2a7b30aea518a808081cf49ca7303d72261
DesistDaydream <39847025+DesistDaydream@users.noreply.github.com>
2021-11-21 11:27:38 +0800

    2021.11.21

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/DesistDaydream/kubernetes-development
 go 1.16
 
 require (
-	k8s.io/apimachinery v0.19.2
-	k8s.io/client-go v0.19.2
+	k8s.io/apimachinery v0.19.16
+	k8s.io/client-go v0.19.16
 )
---
+++ node_handle/node_handle.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	listers "k8s.io/client-go/listers/core/v1"
+)
+
+type nodeHandel struct {
+	nodeStore listers.NodeLister
+}
+
+// InOrOut 判断当前环境是在集群内部，还是集群外部
+func InOrOut() string {
+	// 如果容器内具有环境变量 KUBERNETES_SERVICE_HOST 且不为空，则当前代码是在容器内运行，否则是在集群外部运行
+	if h := os.Getenv("KUBERNETES_SERVICE_HOST"); h != "" {
+		return "inCluster"
+	}
+	return "outCluster"
+}
+
+// getDeployment 获取指定 namespace 下所有的 deployment 对象
+func getNode(clientset *kubernetes.Clientset, nodeName string) {
+	var nh nodeHandel
+	cacheNode, _ := nh.nodeStore.Get(nodeName)
+	node := cacheNode.DeepCopy()
+	// 获取指定 名称空间 下所有的 deployment 对象
+	// node, _ := clientset.CoreV1().Nodes().Get(context.TODO(), nodeName, v1.GetOptions{})
+	fmt.Printf("%v\n", node.Spec.PodCIDR)
+}
+
+func main() {
+	var config *rest.Config
+	// 根据代码所在环境，决定如何创建一个连接集群所需的配置。
+	switch InOrOut() {
+	case "inCluster":
+		// 根据容器内的 /var/run/secrets/kubernetes.io/serviceaccount/ 目录下的 token 与 ca.crt 文件创建一个用于连接集群的配置。
+		config, _ = rest.InClusterConfig()
+	case "outCluster":
+		// 根据指定的 kubeconfig 文件创建一个用于连接集群的配置，/root/.kube/config 为 kubectl 命令所用的 config 文件
+		config, _ = clientcmd.BuildConfigFromFlags("", "/root/.kube/config")
+		// 注意，clientcmd.BuildConfigFromFlags() 内部实际上也是有调用 rest.InClusterConfig() 的逻辑，只要满足条件即可。条件如下：
+		// 若第二个参数为空的话，则会直接调用 rest.InClusterConfig()
+	}
+
+	// 根据 BuildConfigFromFlags 创建的配置，返回一个可以连接集群的指针
+	clientset, _ := kubernetes.NewForConfig(config)
+
+	// 获取指定 namespace 下所有的 deployment 对象
+	getNode(clientset, "node-1.bj-test")
+}
